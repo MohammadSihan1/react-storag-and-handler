8929383ce70136c63abd7784ccdeb804fd7066bf
MohammadSihan1 <mohammadsihan289@gmail.com>
2023-03-26 11:01:59 +0600

    event handler added

--- src/components/cosmetic/cosmetic.js
+++ src/components/cosmetic/cosmetic.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import './cosmetic.css';
 
 
-const Cosmetic= (props) => {
-    const {name, price, id} = props.cosmetic;
-    return(
-        <div>
+const Cosmetic = (props) => {
+    const { name, price, id } = props.cosmetic;
+    // event handler:
+    const addToCart = (id) => {
+        console.log('item added', id);
+    }
+
+    const addToCartWithParam = () => addToCart(id)
+    return (
+        <div className='product'>
             <h2>
                 buy this: {name}
             </h2>
@@ -14,6 +21,10 @@ const Cosmetic= (props) => {
             <h2>
                 <small>it has an id:{id}</small>
             </h2>
+            <button onClick={addToCartWithParam}>Add to Cart</button>
+            {/* Shortcut technique:----
+                                      |*/}
+            <button onClick={() => addToCart(id)}>Purchase</button>
         </div>
     )
 }
